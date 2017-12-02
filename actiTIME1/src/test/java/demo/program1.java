05cf3af82e61054eafebafa4fac6debf9f049fe4
admin <admin@qspider>
2017-12-01 23:07:15 -0800

    deonee

---
+++ actiTIME1/src/test/java/demo/program1.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class program1 
+{
+	@Test
+	public void m1()
+	{
+		Reporter.log("byeeeee" , true);
+	}
+}
